8f6c8a1c4050c9ec935c60e9a49efe3fe0cb88b2
-akku- <151517253+akku1139@users.noreply.github.com>
2025-07-15 10:40:47 +0900

    setup

---
+++ _akku/update.sh
@@ -0,0 +1,28 @@
+sudo mkdir /mnt/work
+sudo chown runner:runner -R /mnt/work
+
+targets=($(find system vendor -type f -name "*.json"))
+
+for target in $targets; do
+  echo "target" $target
+  cmd="python _akku/save.py \"$target\""
+
+  while true; do
+    out=($($cmd))
+    status=$?
+
+    if [ "$status" -eq 0 ]; then
+      wget -O /mnt/work/${out[2]} ${out[1]}
+
+      git add -A
+      git commit -m "Update"
+      git push
+
+      gh release create dl-${out[0]} "/mnt/work/${out[2]}"
+    else
+      echo "downloading next file..."
+      break
+    fi
+  done
+
+done
