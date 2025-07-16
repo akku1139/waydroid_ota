88e2133a43d7b853e990ca41e6dee9c1daba8940
-akku- <151517253+akku1139@users.noreply.github.com>
2025-07-16 20:10:23 +0900

    new download manager

--- _akku/update.sh
+++ _akku/update.sh
@@ -9,13 +9,81 @@ targets=$(find system vendor -type f -name "*.json")
 
 ## Download manager
 
-# key: filename, value: url
-declare -A FILENAME_URL
+MAX_JOBS=10
+
+# key: filename
+declare -A FILENAME_URL # value: url
+declare -A FILENAME_PID # value: pid
+
+# filename, FIFO
+declare -a JOB_QUEUE=()
+
+# init semaphore
+SEMAPHORE="/run/user/$(id -u)/dl_semaphore_$$"
+mkfifo "$SEMAPHORE" || { echo "Error: Couldn't create semaphore"; exit 1; }
+exec 3<> "$SEMAPHORE"
+
+for ((i=0; i<MAX_JOBS; i++)); do
+  echo "$i" >&3
+done
+
+wget_job() {
+  filename=$1
+  url=${FILENAME_URL[$filename]}
+  job_id=$2
+  if [ "$job_id" = "" ]; then
+    echo "[job] Start downloading '$filename' (from outside the job queue)"
+  else
+    echo "[job] Start downloading '$filename' (id: $job_id)"
+  fi
+  wget -nv -O /mnt/work/$filename $url
+  unset FILENAME_URL[$filename]
+  unset FILENAME_PID[$filename]
+  echo "[job] '$filename' download has been completed"
+}
+
+dispatcher() {
+  while true; do
+    filename=${JOB_QUEUE[0]}
+    if [ "$filename" = "" ]; then
+      echo "[dispatcher] All jobs have been run. stopping."
+      break
+    fi
+    JOB_QUEUE=${JOB_QUEUE[@]:1}
+    read job_id <&3
+    echo "[dispatcher] Starting a job (id: $job_id)"
+    wget_job $filename $job_id &
+    JOB_PID=$!
+    FILENAME_PID[$filename]=$JOB_PID
+    echo "$job_id" >&3 # Return token to the semaphore
+  done
+}
 
 wait_for_file_foreground() {
   filename=$1
-  url=${FILENAME_URL[$filename]}
-  wget -nv -O /mnt/work/$filename $url
+  pid=${FILENAME_PID[$filename]}
+  
+  if [ "$pid" = ""]; then
+    if [ "${FILENAME_URL[$filename]}" = "" ]; then
+      echo "[fg] the job is already done (filename: $filename)"
+    else
+      echo "[fg] the job is waiting (filename: $filename)"
+      # remove from the queue
+      for i in "${!JOB_QUEUE[@]}" ; do
+        if [ "${JOB_QUEUE[$i]}" = "$filename" ]; then
+          unset JOB_QUEUE[$i]
+        fi
+      done
+      JOB_QUEUE=(${JOB_QUEUE[@]})
+      echo "[fg] started the job (filename: $filename)"
+      wget_job $filename
+      echo "[fg] the job is complete (filename: $filename)"
+    fi
+  else
+    echo "[fg] the job is running. waiting for completion... (filename $filename)"
+    wait $pid
+    echo "[fg] the job is complete (filename: $filename)"
+  fi
 }
 
 ## End
@@ -29,6 +97,7 @@ for target in $targets; do
     [ -z "$url" ] && continue
 
     FILENAME_URL[$filename]=$url
+    JOB_QUEUE+=($filename)
     echo "added a job to queue: $filename"
   done < <(python _akku/files.py $target)
 
@@ -54,7 +123,7 @@ for target in $targets; do
     bname="dl/$id"
 
     if [ "$status" -eq 0 ]; then
-      echo downloading
+      echo downloading $filename
       # wget -nv -O /mnt/work/$filename $url
       # aria2c -x10 -s10 --console-log-level=warn -o /mnt/work/$filename $url
       wait_for_file_foreground $filename
